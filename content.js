99365590dbea981d2dc8c427ec36534e60735526
Quin Outland <qoutland@gmail.com>
2019-01-18 14:28:04 -0800

    Removed unneccesary files

--- content.js
+++
@@ -1,2 +0,0 @@
-// content.js
-console.log("This is a test")
--- play.js
+++
@@ -1,6 +0,0 @@
-try {
-    document.getElementsByClassName('control-button spoticon-pause-16')[0].click();
-}
-catch(err){
-    document.getElementsByClassName('control-button spoticon-play-16')[0].click();
-}"
